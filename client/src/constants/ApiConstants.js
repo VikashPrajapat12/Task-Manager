b0c7fa297c123bb493b59ad47c2eb17817e3fe3e
Vikash <vikashprajapat450@gmail.com>
2023-07-13 16:02:53 +0530

    update Url

--- client/src/constants/ApiConstants.js
+++ client/src/constants/ApiConstants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5500"
+export const BASE_URL = "https://task-manger-server-dr2o.onrender.com"      // for deploying live project(Backend)
 export const TASK_URL_PREFIX = BASE_URL + '/tasks'
 export const USER_URL_PREFIX = BASE_URL + '/users'
